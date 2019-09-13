80ffa0537ca09172de8f6203825cb280e9a071ab
Xavier Solau <xaviersolau@yahoo.fr>
2019-09-14 00:49:33 +0200

    Add SoloX.CodeQuality.Test.Helpers with process and dotnet helpers.

---
+++ src/libs/SoloX.CodeQuality.Test.Helpers/DotnetHelper.cs
@@ -0,0 +1,47 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="DotnetHelper.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoloX.CodeQuality.Test.Helpers
+{
+    public static class DotnetHelper
+    {
+        private const string DOTNET = "dotnet";
+        private const string RESTORE = "restore";
+        private const string BUILD = "build";
+        private const string PUBLISH = "publish";
+        private const string RUN = "run";
+
+        public static bool Restore(string projectPath, out string stdout, out string stderr)
+        {
+            return ProcessHelper.Run(projectPath, DOTNET, RESTORE, out stdout, out stderr) == 0;
+        }
+
+        public static bool Build(string projectPath, out string stdout, out string stderr)
+        {
+            return ProcessHelper.Run(projectPath, DOTNET, BUILD, out stdout, out stderr) == 0;
+        }
+
+        public static bool Publish(string projectPath, out string stdout, out string stderr)
+        {
+            return ProcessHelper.Run(projectPath, DOTNET, PUBLISH, out stdout, out stderr) == 0;
+        }
+
+        public static bool Run(string projectPath, out string stdout, out string stderr)
+        {
+            return Run(projectPath, string.Empty, out stdout, out stderr);
+        }
+
+        public static bool Run(string projectPath, string args, out string stdout, out string stderr)
+        {
+            return ProcessHelper.Run(projectPath, DOTNET, $"{RUN} {args}", out stdout, out stderr) == 0;
+        }
+    }
+}
---
+++ src/libs/SoloX.CodeQuality.Test.Helpers/ProcessHelper.cs
@@ -0,0 +1,49 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="ProcessHelper.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SoloX.CodeQuality.Test.Helpers
+{
+    /// <summary>
+    /// ProcessHelper allows to run a process and to get the output.
+    /// </summary>
+    public static class ProcessHelper
+    {
+        /// <summary>
+        /// Run a process with the given arguments and wait for exit.
+        /// </summary>
+        /// <param name="workingDirectory">Working directory.</param>
+        /// <param name="command">The command to run.</param>
+        /// <param name="arguments">The command arguments.</param>
+        /// <param name="stdout">Standard output.</param>
+        /// <param name="stderr">Error output.</param>
+        /// <returns>The process exit code.</returns>
+        public static int Run(string workingDirectory, string command, string arguments, out string stdout, out string stderr)
+        {
+            var dotnet = new ProcessStartInfo(command, arguments)
+            {
+                WorkingDirectory = workingDirectory,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+            };
+            using (var process = Process.Start(dotnet))
+            {
+                process.WaitForExit();
+
+                stdout = process.StandardOutput.ReadToEnd();
+                stderr = process.StandardError.ReadToEnd();
+
+                return process.ExitCode;
+            }
+        }
+    }
+}
