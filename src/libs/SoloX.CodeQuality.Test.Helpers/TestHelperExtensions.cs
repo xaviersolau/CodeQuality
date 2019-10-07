6ecec41d9d96c270280d1fe8e5edcbc151e7bede
Xavier Solau <xaviersolau@yahoo.fr>
2019-10-08 00:05:20 +0200

    Add Test Helper Extensions that register the TestLogging service.

---
+++ src/libs/SoloX.CodeQuality.Test.Helpers/TestHelperExtensions.cs
@@ -0,0 +1,37 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="TestHelperExtensions.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SoloX.CodeQuality.Test.Helpers.Logger;
+using Xunit.Abstractions;
+
+namespace SoloX.CodeQuality.Test.Helpers
+{
+    public static class TestHelperExtensions
+    {
+        /// <summary>
+        /// Setup the test logging service on the given ServiceCollection.
+        /// </summary>
+        /// <param name="serviceCollection">The service collection to setup.</param>
+        /// <param name="testOutputHelper">The XUnit test output helper.</param>
+        /// <returns>The given service collection.</returns>
+        public static IServiceCollection AddTestLogging(
+            this IServiceCollection serviceCollection,
+            ITestOutputHelper testOutputHelper)
+        {
+            serviceCollection.Add(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(TestLogger<>)));
+            return serviceCollection
+                .AddSingleton(testOutputHelper)
+                .AddSingleton<ILoggerFactory, TestLoggerFactory>()
+                .AddSingleton<ILogger, TestLogger<object>>();
+        }
+    }
+}
