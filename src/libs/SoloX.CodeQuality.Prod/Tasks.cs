89e5d9c985e1d6d4aaf8701892b493b4d13ea87d
Xavier Solau <xaviersolau@yahoo.fr>
2021-03-04 22:58:22 +0100

    Fix cross platform build.

---
+++ src/libs/SoloX.CodeQuality.Prod/Tasks.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace SoloX.CodeQuality.Prod
+{
+    public class Tasks
+    {
+        public string InputFilename { get; set; }
+        public string OutputFilename { get; set; }
+        public bool Overwrite { get; set; }
+        public void CopyTextFile()
+        {
+            if (Overwrite)
+            {
+                File.WriteAllText(OutputFilename, File.ReadAllText(InputFilename));
+            }
+            else
+            {
+                File.AppendAllText(OutputFilename, File.ReadAllText(InputFilename));
+            }
+        }
+    }
+}
