517751ce5d6ae060ff58ebd82a90a085a2eed55b
Xavier Solau <xaviersolau@yahoo.fr>
2021-08-03 08:42:46 +0200

    Update HttpClientMockBuilder to handle HTTP method.

--- src/libs/SoloX.CodeQuality.Test.Helpers/Http/HttpClientMockBuilder.cs
+++ src/libs/SoloX.CodeQuality.Test.Helpers/Http/HttpClientMockBuilder.cs
@@ -1,5 +1,5 @@
 ﻿// ----------------------------------------------------------------------
-// <copyright file="HttpClientBuilder.cs" company="Xavier Solau">
+// <copyright file="HttpClientMockBuilder.cs" company="Xavier Solau">
 // Copyright © 2021 Xavier Solau.
 // Licensed under the MIT license.
 // See LICENSE file in the project root for full license information.
@@ -20,7 +20,7 @@ namespace SoloX.CodeQuality.Test.Helpers.Http
     /// </summary>
     public class HttpClientMockBuilder
     {
-        private Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>> responseMap = new Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>>();
+        private Dictionary<string, Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>>> responseMap = new Dictionary<string, Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>>>();
         private Uri baseAddress;
 
         /// <summary>
@@ -28,39 +28,93 @@ namespace SoloX.CodeQuality.Test.Helpers.Http
         /// </summary>
         /// <param name="baseAddress">Base address to use.</param>
         /// <returns>The current builder.</returns>
+        /// <remarks>baseAddress must not contain query string.</remarks>
         public HttpClientMockBuilder WithBaseAddress(Uri baseAddress)
         {
-            this.baseAddress = baseAddress;
+            if (baseAddress is null)
+            {
+                throw new ArgumentNullException(nameof(baseAddress));
+            }
+
+            if (!string.IsNullOrEmpty(baseAddress.Query))
+            {
+                throw new ArgumentException($"{nameof(baseAddress)} should not contain query data");
+            }
+
+            if (baseAddress.AbsolutePath.EndsWith("/", StringComparison.Ordinal))
+            {
+                this.baseAddress = baseAddress;
+            }
+            else
+            {
+                this.baseAddress = new Uri($"{baseAddress}/");
+            }
+
             return this;
         }
 
         /// <summary>
         /// Setup a response builder for the given absolute path request.
         /// </summary>
-        /// <param name="absolutePath">The absolute path request to match.</param>
+        /// <param name="absolutePath">The absolute path request to match. (It must start with '/')</param>
         /// <param name="responseBuilder">The response builder.</param>
+        /// <param name="httpMethod">The HTTP method.</param>
         /// <returns>The current builder.</returns>
-        public HttpClientMockBuilder WithResponse(string absolutePath, Action<HttpRequestMessage, HttpResponseMessage> responseBuilder)
+        public HttpClientMockBuilder WithResponse(string absolutePath, Action<HttpRequestMessage, HttpResponseMessage> responseBuilder, string httpMethod = "GET")
         {
-            this.responseMap.Add(absolutePath, responseBuilder);
+            if (absolutePath is null)
+            {
+                throw new ArgumentNullException(nameof(absolutePath));
+            }
+
+            if (responseBuilder is null)
+            {
+                throw new ArgumentNullException(nameof(responseBuilder));
+            }
+
+            if (httpMethod is null)
+            {
+                throw new ArgumentNullException(nameof(httpMethod));
+            }
+
+            if (!absolutePath.StartsWith("/", StringComparison.Ordinal))
+            {
+                throw new ArgumentException($"{nameof(absolutePath)} should start with '/'");
+            }
+
+            httpMethod = httpMethod.ToUpperInvariant();
+
+            if (!this.responseMap.TryGetValue(httpMethod, out var map))
+            {
+                map = new Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>>();
+                this.responseMap.Add(httpMethod, map);
+            }
+
+            map.Add(absolutePath, responseBuilder);
+
             return this;
         }
 
         /// <summary>
         /// Setup a OK response for the given absolute path request.
         /// </summary>
-        /// <param name="absolutePath">The absolute path request to match.</param>
+        /// <param name="absolutePath">The absolute path request to match. (It must start with '/')</param>
         /// <param name="responseContentBuilder">The response content builder.</param>
+        /// <param name="httpMethod">The HTTP method.</param>
         /// <returns>The current builder.</returns>
-        public HttpClientMockBuilder WithOkResponse(string absolutePath, Func<HttpRequestMessage, HttpContent> responseContentBuilder)
+        public HttpClientMockBuilder WithOkResponse(string absolutePath, Func<HttpRequestMessage, HttpContent> responseContentBuilder, string httpMethod = "GET")
         {
             return WithResponse(
                 absolutePath,
                 (request, response) =>
                 {
                     response.StatusCode = HttpStatusCode.OK;
-                    response.Content = responseContentBuilder(request);
-                });
+                    if (responseContentBuilder != null)
+                    {
+                        response.Content = responseContentBuilder(request);
+                    }
+                },
+                httpMethod);
         }
 
         /// <summary>
@@ -83,14 +137,14 @@ namespace SoloX.CodeQuality.Test.Helpers.Http
         private void Reset()
         {
             this.baseAddress = null;
-            this.responseMap = new Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>>();
+            this.responseMap = new Dictionary<string, Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>>>();
         }
 
         private class HttpMessageHandlerMock : HttpMessageHandler
         {
-            private readonly Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>> responseMap;
+            private readonly Dictionary<string, Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>>> responseMap;
 
-            public HttpMessageHandlerMock(Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>> responseMap)
+            public HttpMessageHandlerMock(Dictionary<string, Dictionary<string, Action<HttpRequestMessage, HttpResponseMessage>>> responseMap)
             {
                 this.responseMap = responseMap;
             }
@@ -98,7 +152,9 @@ namespace SoloX.CodeQuality.Test.Helpers.Http
             protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
             {
                 var response = new HttpResponseMessage();
-                if (this.responseMap.TryGetValue(request.RequestUri.AbsolutePath, out var responseBuilder))
+
+                if (this.responseMap.TryGetValue(request.Method.Method.ToUpperInvariant(), out var map)
+                    && map.TryGetValue(request.RequestUri.AbsolutePath, out var responseBuilder))
                 {
                     responseBuilder(request, response);
                 }
